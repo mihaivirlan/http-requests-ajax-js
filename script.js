39931797343d5b768b30a49cb4d9e3309eeed949
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-14 01:04:48 +0300

    POST Request

--- script.js
+++ script.js
@@ -51,3 +51,20 @@
 // };
 // http.send();
 
+
+
+    //POST Request
+// var http = new XMLHttpRequest();
+// var url = 'http://jsonplaceholder.typicode.com/posts';
+// var method = 'POST';
+// var data = 'title=Post%20Title&body=Body'; //%20 - represent a white space
+// http.open(method, url);
+// http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+// http.onreadystatechange = function() {
+//     if (http.readyState === XMLHttpRequest.DONE && http.status === 201) {
+//         console.log(JSON.parse(http.responseText));
+//     } else if (http.readyState === XMLHttpRequest.DONE && http.status !== 201) {
+//         console.log('Error!'); 
+//     }
+// };
+// http.send(data);
