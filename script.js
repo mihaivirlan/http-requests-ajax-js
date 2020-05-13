9673845b2eaefa868c9e1aa60325abc536543b1e
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-14 00:56:38 +0300

    Setup and Sending a GET Request

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
-//Setup and Sending a GET Request
-var http = new XMLHttpRequest();
-var url = 'http://jsonplaceholder.typicode.com/posts';
-var method = 'GET';
-http.open(method, url);
+    //Setup and Sending a GET Request
+// var http = new XMLHttpRequest();
+// var url = 'http://jsonplaceholder.typicode.com/posts';
+// var method = 'GET';
+// http.open(method, url);
+// http.onreadystatechange = function() {
+//     if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+//         console.log(http.responseText);
+//     } else if (http.readyState === XMLHttpRequest.DONE && http.status !== 200) {
+//         console.log('Error!'); 
+//     }
+// };
+// http.send();
+
+// var http = new XMLHttpRequest();
+// var url = 'http://jsonplaceholder.typicode.com/posts';
+// var method = 'GET';
+// http.open(method, url);
+// http.onreadystatechange = function() {
+//     if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+//         console.log(JSON.parse(http.responseText));
+//     } else if (http.readyState === XMLHttpRequest.DONE && http.status !== 200) {
+//         console.log('Error!'); 
+//     }
+// };
+// http.send();
+
+// var http = new XMLHttpRequest();
+// var url = 'http://jsonplaceholder.typicode.com/posts';
+// var method = 'GET';
+// http.open(method, url);
+// http.onreadystatechange = function() {
+//     if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+//         console.log(JSON.parse(http.responseText)[0].id);
+//     } else if (http.readyState === XMLHttpRequest.DONE && http.status !== 200) {
+//         console.log('Error!'); 
+//     }
+// };
+// http.send();
+
+// var http = new XMLHttpRequest();
+// var url = 'http://jsonplaceholder.typicode.com/posts';
+// var method = 'GET';
+// http.open(method, url);
+// http.onreadystatechange = function() {
+//     if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+//         console.log(JSON.parse(http.responseText)[1].title);
+//     } else if (http.readyState === XMLHttpRequest.DONE && http.status !== 200) {
+//         console.log('Error!'); 
+//     }
+// };
+// http.send();
+
